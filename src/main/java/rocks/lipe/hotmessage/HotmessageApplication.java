ec36ee11248cb99df80e716cc79a309c9bbe2950
Felipe Faria <felipesf@ciandt.com>
2017-12-01 15:20:55 -0200

    initial commit

---
+++ src/main/java/rocks/lipe/hotmessage/HotmessageApplication.java
@@ -0,0 +1,12 @@
+package rocks.lipe.hotmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmessageApplication.class, args);
+	}
+}
