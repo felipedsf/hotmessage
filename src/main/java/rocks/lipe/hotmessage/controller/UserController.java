9a9772fb44d3e471c5dd93e9a04ed5feb7129d06
Felipe Faria <felipe@localhost.localdomain>
2017-12-03 14:31:11 -0200

    add swagger

--- src/main/java/rocks/lipe/hotmessage/controller/UserController.java
+++ src/main/java/rocks/lipe/hotmessage/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import rocks.lipe.hotmessage.domain.User;
 import rocks.lipe.hotmessage.service.UserService;
 
@@ -18,11 +19,13 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	@ApiOperation(value = "Register user", notes = "Register user in the database")
 	@PostMapping("/register")
 	public void register(@RequestBody User user) {
 		userService.registerUser(user.getName(), user.getPassword());
 	}
 
+	@ApiOperation(value = "Login", notes = "Verify if the user exists in the database")
 	@PostMapping("/login")
 	public User login(@RequestBody User user) {
 		return userService.getUser(user.getName(), user.getPassword());
